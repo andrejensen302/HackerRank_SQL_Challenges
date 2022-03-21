09c39148baf1a613aed1f3f2a97a8ea993b7c965
Andre Jensen <andrejensen302@gmail.com>
2022-03-20 19:04:45 -0700

    Created folder for Advanced Select Challenges. Upload solutions

---
+++ Aggregation Challenges/The-Blunder.sql
@@ -0,0 +1,9 @@
+/*
+Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table,
+but did not realize her keyboard's 0  key was broken until after completing the calculation.
+She wants your help finding the difference between her miscalculation (using salaries with any zeros removed), and the actual average salary.
+Write a query calculating the amount of error (i.e.: actual - miscalculated  average monthly salaries), and round it up to the next integer.
+*/
+
+SELECT CEILING(AVG(SALARY) - AVG(REPLACE(SALARY,'0','')))
+FROM EMPLOYEES;
